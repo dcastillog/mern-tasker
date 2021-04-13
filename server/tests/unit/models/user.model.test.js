ff99a12793bb372a011ab6566fff72e3b4694983
dcastillog <diego.castilloguajardo@gmail.com>
2021-04-13 19:29:08 -0400

    test(server): add tests for authentication and unit test for user model

---
+++ server/tests/unit/models/user.model.test.js
@@ -0,0 +1,57 @@
+const faker = require('faker');
+const { User } = require('../../../src/models');
+
+describe('User model', () => {
+  describe('User validation', () => {
+    let newUser;
+    beforeEach(() => {
+      newUser = {
+        name: faker.name.findName(),
+        email: faker.internet.email().toLocaleLowerCase(),
+        password: 'pass1234#tasker',
+        role: 'user',
+      };
+    });
+
+    test('should correctly validate a valid user', async () => {
+      await expect(new User(newUser).validate()).resolves.toBeUndefined();
+    });
+
+    test('should throw a validation error if email is invalid', async () => {
+      newUser.email = 'invalid email';
+      await expect(new User(newUser).validate()).rejects.toThrow();
+    });
+
+    test('should throw a validation error if password length it less than 8 characters', async () => {
+      newUser.password = 'passwo1';
+      await expect(new User(newUser).validate()).rejects.toThrow();
+    });
+
+    test('should throw a validation error if password does not contain numbers', async () => {
+      newUser.password = 'password';
+      await expect(new User(newUser).validate()).rejects.toThrow();
+    });
+
+    test('should throw a validation error if password does not contain letters', async () => {
+      newUser.password = '11111111';
+      await expect(new User(newUser).validate()).rejects.toThrow();
+    });
+
+    test('should thorw a validation error if role is unkown', async () => {
+      newUser.role = 'unkown';
+      await expect(new User(newUser).validate()).rejects.toThrow();
+    });
+  });
+
+  describe('User toJSON()', () => {
+    test('should not return user password when toJSON is called', () => {
+      const newUser = {
+        name: faker.name.findName(),
+        email: faker.internet.email().toLocaleLowerCase(),
+        password: 'pass1234#tasker',
+        role: 'user',
+      };
+      expect(new User(newUser).toJSON()).not.toHaveProperty('password');
+    });
+  });
+});
